d8cf45a90a6fa97ab70df519de27bf9af427a457
Yaoxing <yaoxing.zhang@gmail.com>
2017-01-04 21:48:08 +0800

    Update setup.sh
    * image from 163 is using a different path to install askbot package

--- askbot/setup.sh
+++ askbot/setup.sh
@@ -2,8 +2,8 @@
 
 cd /opt/ask
 
-sed -i s/user_id=/id=/g /usr/lib/python2.7/site-packages/askbot-0.10.1-py2.7.egg/askbot/deps/django_authopenid/views.py
-sed -i s/user_id\)/id\)/g /usr/lib/python2.7/site-packages/askbot-0.10.1-py2.7.egg/askbot/deps/django_authopenid/views.py
+sed -i s/user_id=/id=/g /usr/local/lib/python2.7/site-packages/askbot-0.10.1-py2.7.egg/askbot/deps/django_authopenid/views.py
+sed -i s/user_id\)/id\)/g /usr/local/lib/python2.7/site-packages/askbot-0.10.1-py2.7.egg/askbot/deps/django_authopenid/views.py
 
 python manage.py syncdb
 python manage.py migrate
